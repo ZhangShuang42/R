110278250eedea69150270a64e51315e888ee127
ZhangShuang42 <33388389+ZhangShuang42@users.noreply.github.com>
2017-11-16 20:32:41 +0800

    Update Examples.R

--- Examples.R
+++ Examples.R
@@ -10,3 +10,18 @@ exact=F,correct=F,confi.int=T)
 data:  x
 V = 53.5, p-value = 0.03411
 alternative hypothesis: true location is greater than 12
+
+#Brown-Mood中位数检验
+x=c(11:20,40,60)
+length(x)
+y=c(3:10,30,50)
+length(y)
+z=c(x,y)
+length(z)
+m=length(x)
+n=length(y)
+mxy=median(z)
+a=sum(x>mxy)  #a=9
+b=sum(y>mxy)  #b=2
+p=phyper(b,n,m,a+b) #p=0.0149865614261899<0.05，所以我们拒绝H0，认为Mx>My
+p=phyper(a,m,n,a+b) #当a<b时，即H1:Mx<My
