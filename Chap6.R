71418acc4eec3d4da28c2d20066be084a4da06fe
ZhangShuang42 <33388389+ZhangShuang42@users.noreply.github.com>
2017-12-07 21:13:50 +0800

    Create Chap6.R

---
+++ Chap6.R
@@ -0,0 +1,22 @@
+#Spearman & Kendall 相关检验
+x=c(45,52,54,63,62,68,75,76,92,88)
+y=c(100,111,120,133,140,152,160,171,180,195)
+cor.test(x,y,method = "spearman")
+  #结果：
+	Spearman's rank correlation rho
+data:  x and y
+S = 4, p-value < 2.2e-16
+alternative hypothesis: true rho is not equal to 0
+sample estimates:
+      rho 
+0.9757576 
+
+cor.test(x,y,method = "kendall")
+  #结果：
+	Kendall's rank correlation tau
+data:  x and y
+T = 43, p-value = 2.976e-05
+alternative hypothesis: true tau is not equal to 0
+sample estimates:
+      tau 
+0.9111111 
